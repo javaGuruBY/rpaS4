dbc71f066620cdbf24fedb41d3d0d4afa4099cef
FlashSuppressoR <Boomking90@gmail.com>
2021-03-01 21:52:39 +0300

    HJ2T4: Introduced changes after review

--- homework/java/PilipenkoAlex/lecture/lecture2/RandomGeneratorDemo.java
+++ homework/java/PilipenkoAlex/lecture/lecture2/RandomGeneratorDemo.java
@@ -1,22 +1,18 @@
 package homework.java.PilipenkoAlex.lecture.lecture2;
 
 public class RandomGeneratorDemo {
-    int bottomValueBoundary = 0;  //нижняя граница случайного значения
-    int upperValueBoundary = 100; //верхняя граница случайного значения
-
     public static void main(String[] args) {
-        RandomGeneratorDemo randomGeneratorDemo = new RandomGeneratorDemo();
-        int counter = 1;
         int sum = 0;
-        for(int i = 0; i < 3; i++){
-            int a =  randomGeneratorDemo.randomizer();
-            System.out.println(counter + " случайное число = " + a);
-            counter++;
-            sum += a; //это тоже самое, но более краткое решение, чем - sum = sum + a;
+        for(int i = 1; i < 4; i++){
+            int a = randomizer();
+            System.out.println(i + " случайное число = " + a);
+            sum += a;
         }
         System.out.println("Сумма 3 случайных чисел = " + sum);
     }
-    public int randomizer(){
+    public static int randomizer(){
+        int bottomValueBoundary = 0;  //нижняя граница случайного значения
+        int upperValueBoundary = 100; //верхняя граница случайного значения
         int randomNumb = bottomValueBoundary + (int) (Math.random() * upperValueBoundary);
         return randomNumb;
     }
