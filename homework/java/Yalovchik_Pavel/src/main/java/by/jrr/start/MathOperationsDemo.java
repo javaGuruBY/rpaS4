9bc8c64ff907a7e82af02194b29ecb70a937c4e7
elupen <elupen@users.noreply.github.com>
2021-03-01 23:39:28 +0300

    MathOperationsDemo and RandomGeneratorDemo has been complete

---
+++ homework/java/Yalovchik_Pavel/src/main/java/by/jrr/start/MathOperationsDemo.java
@@ -0,0 +1,21 @@
+package by.jrr.start;
+
+
+public class MathOperationsDemo {
+
+    public static void main(String[] args)  {
+        int a = 15;
+        int b = 5;
+        int addition = a + b;
+        int subtraction = a - b;
+        int division = a / b;
+        int multiplication = a * b;
+
+        System.out.println("addition : " + a + "+" + b + " = " + addition);
+        System.out.println("subtraction : " + a + "-" + b + " = " + subtraction);
+        System.out.println("division : " + a + "/" + b + " = " + division);
+        System.out.println("multiplication : " + a + "x" + b + " = " + multiplication);
+
+
+    }
+}
