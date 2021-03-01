65acc747c1d35bc3611ae2bd91be8d60aeb2b5cb
Татьяна <79410832+FancyTanya@users.noreply.github.com>
2021-03-01 16:40:12 +0200

    random numbers with For loop

--- homework/java/RandomGeneratorDemo.java
+++ homework/java/RandomGeneratorDemo.java
@@ -1,18 +1,19 @@
-package Lessons;
-
 import java.util.Random;
 
 public class RandomGeneratorDemo {
     public static void main(String[] args) {
-        Random random = new Random();
-        int firstRandom = random.nextInt(5) + 1;
-        int secondRandom = random.nextInt(5) + 1;
-        int thirdRandom = random.nextInt(5) + 1;
-        int additionOfRandomNums = firstRandom + secondRandom + thirdRandom;
-        System.out.println("firstRandom = " + firstRandom);
-        System.out.println("secondRandom = " + secondRandom);
-        System.out.println("thirdRandom = " + thirdRandom);
-        System.out.println("additionOfRandomNums = " + additionOfRandomNums);
+        random();
     }
 
+    public static void random() {
+        Random random = new Random();
+        int sum = 0;
+        int max = 10;
+        for (int i = 1; i <= 3; i++) {
+            int randomNum = random.nextInt(max) + 1;
+            System.out.println("Число #" + i + " " + randomNum);
+            sum += randomNum;
+        }
+        System.out.println("Сумма случайных чисел равна " + sum);
+    }
 }
