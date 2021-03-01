c25118200da455a85377a6b3ea60260c16311813
FlashSuppressoR <Boomking90@gmail.com>
2021-03-01 21:26:10 +0300

    HJ2T3: Introduced changes after review

--- homework/java/PilipenkoAlex/lecture/lecture2/MathOperationsDemo.java
+++ homework/java/PilipenkoAlex/lecture/lecture2/MathOperationsDemo.java
@@ -5,7 +5,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MathOperationsDemo {
-
+    public static void main(String[] args) throws IOException {
+        startApplication();
+    }
+    public static void startApplication() throws IOException {
+        System.out.println("Введите 2 числа");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        reader.close();
+        MathOperationsDemo mathOperationsDemo = new MathOperationsDemo();
+        mathOperationsDemo.additionOfNumbers(a, b);
+        mathOperationsDemo.subtractionOfNumbers(a, b);
+        mathOperationsDemo.multiplicationOfNumbers(a, b);
+        mathOperationsDemo.divisionByNumber(a, b);
+    }
     public void additionOfNumbers(int a, int b){
         int sum = a + b;
         System.out.println("Операция сложения двух чисел: " + a + " и " + b + " = " + sum);
@@ -23,18 +37,7 @@ public class MathOperationsDemo {
         System.out.println("Операция деления двух чисел: " + a + " и " + b + " = " + division);
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Введите 2 числа");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
-        reader.close();
-        MathOperationsDemo mathOperationsDemo = new MathOperationsDemo();
-        mathOperationsDemo.additionOfNumbers(a, b);
-        mathOperationsDemo.subtractionOfNumbers(a, b);
-        mathOperationsDemo.multiplicationOfNumbers(a, b);
-        mathOperationsDemo.divisionByNumber(a, b);
-    }
+
 }
 
 
