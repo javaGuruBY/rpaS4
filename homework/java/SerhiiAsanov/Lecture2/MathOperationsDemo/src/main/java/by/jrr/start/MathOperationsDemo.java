123ac99043862d476f8bc101e9d0c457a26d2a98
asanov <serge.asanov@gmail.com>
2021-03-02 07:46:22 +0200

    fix code revue

--- homework/java/SerhiiAsanov/Lecture2/MathOperationsDemo/src/main/java/by/jrr/start/MathOperationsDemo.java
+++ homework/java/SerhiiAsanov/Lecture2/MathOperationsDemo/src/main/java/by/jrr/start/MathOperationsDemo.java
@@ -4,30 +4,28 @@ public class MathOperationsDemo {
 
     public static void main(String[] args) {
 
-        MathOperationsDemo mod = new MathOperationsDemo();
-
         int a = 8;
         int b = 4;
 
-        System.out.println("addition a + b = " + mod.addition(a, b));
-        System.out.println("subtraction a - b = " + mod.subtraction(a, b));
-        System.out.println("multiplication a +* b = " + mod.multiplication(a, b));
-        System.out.println("division a / b = " + mod.division(a, b));
+        System.out.println("addition a + b = " + addition(a, b));
+        System.out.println("subtraction a - b = " + subtraction(a, b));
+        System.out.println("multiplication a +* b = " + multiplication(a, b));
+        System.out.println("division a / b = " + division(a, b));
     }
 
-    public int addition(int a, int b) {
+    public static int addition(int a, int b) {
         return a + b;
     }
 
-    public int subtraction(int a, int b) {
+    public static int subtraction(int a, int b) {
         return a - b;
     }
 
-    public int multiplication(int a, int b) {
+    public static int multiplication(int a, int b) {
         return a * b;
     }
 
-    public int division(int a, int b) {
-        return a / b;
+    public static double division(int a, int b) {
+        return (double) a / b;
     }
 }
