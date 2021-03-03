e73abf1d8dc3593f17ba26b514c5849256b7bf1c
vitalikulsha <kulsha_85@mail.ru>
2021-03-03 19:54:34 +0300

    initial Circle

---
+++ homework/java/VitaliKulsha/Lecture3/src/main/java/circle/Circle.java
@@ -0,0 +1,17 @@
+package circle;
+
+public class Circle {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ homework/java/VitaliKulsha/Lecture3/src/test/java/service/CircleDemoTest.java
@@ -0,0 +1,7 @@
+package service;
+
+import static org.junit.Assert.*;
+
+public class CircleDemoTest {
+
+}
