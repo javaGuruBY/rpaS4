e847c5074c70296a610d214120d79299d031200b
FlashSuppressoR <Boomking90@gmail.com>
2021-03-03 14:23:34 +0300

    Попробовал отменить коммит и повторить

--- homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/by/jrr/service/LoginService.java
+++ homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/by/jrr/service/LoginService.java
@@ -1,4 +1,9 @@
 package homework.java.PilipenkoAlex.lecture.loginFeature.src.main.java.by.jrr.service;
 
+import homework.java.PilipenkoAlex.lecture.loginFeature.src.main.java.by.jrr.bean.User;
+
 public class LoginService {
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- homework/java/PilipenkoAlex/lecture/loginFeature/src/test/java/homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/by/jrr/service/LoginServiceTest.java
+++ homework/java/PilipenkoAlex/lecture/loginFeature/src/test/java/homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/by/jrr/service/LoginServiceTest.java
@@ -1,5 +1,33 @@
-import static org.junit.Assert.*;
+package homework.java.PilipenkoAlex.lecture.loginFeature.src.main.java.by.jrr.service;
+
+import homework.java.PilipenkoAlex.lecture.loginFeature.src.main.java.by.jrr.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
-
+    LoginService loginService;
+    @Before
+    public void setUp(){
+        this.loginService = new LoginService();
+    }
+    @Test
+    public void checkUserPassword_positive(){
+        User user = getUser();
+        String userInput = "password";
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+    @Test
+    public void checkUserPassword_negative(){
+        User user = getUser();
+        String userInput = "wrongPass";
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+    }
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 }
