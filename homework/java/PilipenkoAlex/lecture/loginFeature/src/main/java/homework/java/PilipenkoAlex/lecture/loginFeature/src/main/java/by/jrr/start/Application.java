9affc1aee0a9ecb77b051304f9a3f190f3ef8f35
FlashSuppressoR <Boomking90@gmail.com>
2021-03-03 12:51:31 +0300

    initial commit

---
+++ homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/homework/java/PilipenkoAlex/lecture/loginFeature/src/main/java/by/jrr/start/Application.java
@@ -0,0 +1,8 @@
+package homework.java.PilipenkoAlex.lecture.loginFeature.src.main.java.by.jrr.start;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
