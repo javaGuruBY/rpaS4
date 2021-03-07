077a19a1371a65abb0c905baff27d2c6dd8b58b3
asanov <serge.asanov@gmail.com>
2021-03-08 00:20:42 +0200

    Lecture3 leve2 done

--- homework/java/SerhiiAsanov/Lecture3/Level2/src/main/java/Start/EncoderDemo.java
+++ homework/java/SerhiiAsanov/Lecture3/Level2/src/main/java/Start/EncoderDemo.java
@@ -1,4 +1,10 @@
 package Start;
 
 public class EncoderDemo {
+    public static void main(String[] args) {
+        Encoder encoder = new Encoder();
+
+        encoder.encode((short) 65);
+        encoder.decode('A');
+    }
 }
