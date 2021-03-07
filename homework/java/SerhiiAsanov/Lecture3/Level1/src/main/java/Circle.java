3f0075fa4cebb1c53253e04450455c963eae826f
asanov <serge.asanov@gmail.com>
2021-03-07 23:59:49 +0200

    Lecture4 level1 done

--- homework/java/SerhiiAsanov/Lecture3/Level1/src/main/java/Circle.java
+++ homework/java/SerhiiAsanov/Lecture3/Level1/src/main/java/Circle.java
@@ -1,2 +1,11 @@
 public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * (radius*radius);
+    }
 }
--- homework/java/SerhiiAsanov/Lecture3/Level1/src/main/java/CircleDemo.java
+++ homework/java/SerhiiAsanov/Lecture3/Level1/src/main/java/CircleDemo.java
@@ -1,6 +1,8 @@
 public class CircleDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Circle circle = new Circle(3);
+
+        System.out.println(circle.calculateArea());
     }
 }
