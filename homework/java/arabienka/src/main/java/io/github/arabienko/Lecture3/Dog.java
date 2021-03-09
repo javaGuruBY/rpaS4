06c9e43dcd39bd8a90bfbe44f38424eadccdd40f
Tatsiana Arabiyenka <arabienko@tut.by>
2021-03-09 23:23:44 +0300

    add Privat and toString

--- homework/java/arabienka/src/main/java/io/github/arabienko/Lecture3/Dog.java
+++ homework/java/arabienka/src/main/java/io/github/arabienko/Lecture3/Dog.java
@@ -1,35 +1,45 @@
 package io.github.arabienko.Lecture3;
 
 public class Dog {
-    int age;
-    String color;
-    String name;
-    private boolean dogOne;
+    private int age;
+    private String color;
+    private String name;
+
 
     public void voice() {
-        System.out.println("dog "+name+" age "+age+ " color "+color+" say HAU-HAU");
+        System.out.println("dog " + name + " age " + age + " color " + color + " say HAU-HAU");
 
     }
 
     public void eat() {
-        if (dogOne){
-        System.out.println("dog "+name+" age "+age+ " color "+color+" want to eat");}
-        else System.out.println("dog "+name+" age "+age+ " color "+color+" not hungry");
+        if (name == "Barsik") {
+            System.out.println("dog " + name + " age " + age + " color " + color + " want to eat");
+        } else System.out.println("dog " + name + " age " + age + " color " + color + " not hungry");
     }
 
     public void sleep() {
-        System.out.println("dog "+name+" age "+age+ " color "+color+" want to sleep");
+        System.out.println("dog " + name + " age " + age + " color " + color + " want to sleep");
     }
 
-    public void age(int age) {
-        this.age=age;
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public void color(String color) {
-        this.color=color;
+    public void setColor(String color) {
+        this.color = color;
     }
 
-    public void name(String name) {
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", color='" + color + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- homework/java/arabienka/src/main/java/io/github/arabienko/Lecture3/DogDemo.java
+++ homework/java/arabienka/src/main/java/io/github/arabienko/Lecture3/DogDemo.java
@@ -3,22 +3,25 @@ package io.github.arabienko.Lecture3;
 public class DogDemo {
     public static void main(String[] args) {
         Dog dogOne = new Dog();
-        dogOne.age(12);
-        dogOne.color("brown");
-        dogOne.name("Barsik");
+        dogOne.setAge(12);
+        dogOne.setColor("brown");
+        dogOne.setName("Barsik");
 
         dogOne.voice();
         dogOne.sleep();
         dogOne.eat();
 
         Dog dogTwo = new Dog();
-        dogTwo.age(5);
-        dogTwo.color("white");
-        dogTwo.name("Bobik");
-
+        dogTwo.setAge(5);
+        dogTwo.setColor("white");
+        dogTwo.setName("Bobik");
+        System.out.println();
         dogTwo.voice();
         dogTwo.sleep();
         dogTwo.eat();
+        System.out.println();
+        System.out.println(dogOne.toString());
+        System.out.println(dogTwo.toString());
 
 
     }
