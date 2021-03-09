fc46733b0d3132cbb7280cbb247c19945aa3ac16
KuzmichFather <kuzkinfather@yandex.ru>
2021-03-09 04:53:38 +0300

    adds reduce login attempts on wrong password

--- homework/java/hw_kuzmichfather/loginFeature/src/test/java/by/jrr/bean/service/LoginServiceTest.java
+++ homework/java/hw_kuzmichfather/loginFeature/src/test/java/by/jrr/bean/service/LoginServiceTest.java
@@ -35,7 +35,28 @@ public class LoginServiceTest {
         Assert.assertFalse(actualResult);
     }
 
+    @Test
+    public void reduceLoginAttempts() {
+        loginService.reduceLoginAttempts(user);
+        Assert.assertEquals(2,user.getLoginAttemps());
+    }
 
+    @Test
+    public void login_positive() {
+        String userInput = "password";
+
+        boolean actualResult = loginService.login(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void login_negative() {
+        String userInput = "wrong_password";
+
+        boolean actualResult = loginService.login(user, userInput);
+        Assert.assertFalse(actualResult);;
+        Assert.assertEquals(2,user.getLoginAttemps());
+    }
 
     private User getUser() {
         User user = new User();
